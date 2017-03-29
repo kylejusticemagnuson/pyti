Add Average True Range, Fix True Range

py_technical_indicators/technical_indicators/true_range.py
@@ -2,14 +2,18 @@ import numpy as np
 import catch_errors
 
 
-def true_range(close_data, high_data, low_data):
+def true_range(close_data, period):
     """
     True Range.
-    TRt = MAX(abs(Ht - Lt), abs(Ht - Ct), abs(Lt - Ct-1))
+    TRt = MAX(abs(Ht - Lt), abs(Ht - Ct-1), abs(Lt - Ct-1))
     """
-    catch_errors.check_for_input_len_diff(close_data, high_data, low_data)
+    catch_errors.check_for_period_error(close_data, period)
 
-    tr = map(lambda idx: np.max([abs(high_data[idx] - low_data[idx]), abs(high_data[idx] - close_data[idx]), abs(low_data[idx] - close_data[idx-1])]), range(1, len(close_data)))
+    tr = map(
+        lambda idx:
+        np.max([np.max(close_data[idx+1-period:idx+1]) - np.min(close_data[idx+1-period:idx+1]), abs(np.max(close_data[idx+1-period:idx+1]) - close_data[idx-1]), abs(np.min(close_data[idx+1-period:idx+1]) - close_data[idx-1])]),
+        range(period-1, len(close_data))
+        )
     non_computable_values = np.repeat(np.nan, len(close_data) - len(tr))
     tr = np.append(non_computable_values, tr)
     return tr