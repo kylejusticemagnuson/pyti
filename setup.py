99f30dea47581107ba5be130edf5ed6c50c7b7d1
Kyle Magnuson <kyle@collectiveidea.com>
2017-04-03 13:48:12 -0400

    Remove boilerplate comments

--- setup.py
+++ setup.py
@@ -25,15 +25,11 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
 
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Financial Tools',
 
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
     ],
 
