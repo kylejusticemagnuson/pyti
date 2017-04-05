d44bb3ab95c6e2b16c0c5d836d81fda16f46b664
Kyle Magnuson <kyle@collectiveidea.com>
2017-04-05 13:10:02 -0400

    v0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="pyti",
 
-    version='0.1.2',
+    version='0.1.3',
 
     description='Technical Indicator Library',
     long_description="This library contains various financial technical "
