661b0704072fb88fc9d0c2c9c14c1aae9c14fc84
Vincent Lonij <29819815+vincentropy@users.noreply.github.com>
2018-05-08 12:21:27 +0100

    cleanup for atr and smma

--- pyti/average_true_range.py
+++ pyti/average_true_range.py
@@ -13,13 +13,6 @@ def average_true_range(close_data, period):
     ATRt = ATRt-1 * (n - 1) + TRt / n
     """
     tr = true_range(close_data, period)
-    # atr = np.zeros(len(tr))
-    # atr[0:period-1] = tr[0:period-1]
-    # atr[period-1:period-1+period-1] = tr[0:period-1]
-    # atr[period-1+period-1] = ((1 / float(period)) *
-    #                           sum(tr[period-1:period-1+period])
-    #                           )
-    # for idx in range(period-1+period, len(tr)):
-    #     atr[idx] = (atr[idx-1] * (period - 1) + tr[idx]) / period
-
-    return smoothed_moving_average(tr, period)
+    atr = smoothed_moving_average(tr, period)
+    atr[0:period-1] = tr[0:period-1]
+    return atr
--- pyti/smoothed_moving_average.py
+++ pyti/smoothed_moving_average.py
@@ -14,9 +14,5 @@ def smoothed_moving_average(data, period):
     smma = avg(data(n)) - avg(data(n)/n) + data(t)/n
     """
     catch_errors.check_for_period_error(data, period)
-    # smma = [((np.mean(data[idx-(period-1):idx+1]) -
-    #      (np.mean(data[idx-(period-1):idx+1])/period) +
-    #      data[idx])/period) for idx in range(0, len(data))]
-    # smma = fill_for_noncomputable_vals(data, smma)
     series = pd.Series(data)
     return series.ewm(alpha = 1.0/period).mean().values.flatten()
